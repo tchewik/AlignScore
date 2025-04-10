994d918c453662f6f8f4baf6646037b517163e62
tchewik <elenachistov@gmail.com>
2025-04-10 16:25:13 +0300

    Fix for transformers==4.51.1

--- src/alignscore/model.py
+++ src/alignscore/model.py
@@ -1,13 +1,13 @@
 import math
 from typing import Optional, Tuple
-from transformers import AdamW, get_linear_schedule_with_warmup, AutoConfig
+from torch.optim import AdamW
+from transformers import get_linear_schedule_with_warmup, AutoConfig
 from transformers import BertForPreTraining, BertModel, RobertaModel, AlbertModel, AlbertForMaskedLM, RobertaForMaskedLM
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 from sklearn.metrics import f1_score
 from dataclasses import dataclass
-
 
 
 class BERTAlignModel(pl.LightningModule):
@@ -21,7 +21,7 @@ class BERTAlignModel(pl.LightningModule):
             assert using_pretrained == True, "Only support pretrained muppet!"
             self.base_model = RobertaModel.from_pretrained(model)
             self.mlm_head = RobertaForMaskedLM(AutoConfig.from_pretrained(model)).lm_head
-            
+
         elif 'roberta' in model:
             if using_pretrained:
                 self.base_model = RobertaModel.from_pretrained(model)
@@ -29,7 +29,7 @@ class BERTAlignModel(pl.LightningModule):
             else:
                 self.base_model = RobertaModel(AutoConfig.from_pretrained(model))
                 self.mlm_head = RobertaForMaskedLM(AutoConfig.from_pretrained(model)).lm_head
-            
+
         elif 'albert' in model:
             if using_pretrained:
                 self.base_model = AlbertModel.from_pretrained(model)
@@ -37,7 +37,7 @@ class BERTAlignModel(pl.LightningModule):
             else:
                 self.base_model = AlbertModel(AutoConfig.from_pretrained(model))
                 self.mlm_head = AlbertForMaskedLM(AutoConfig.from_pretrained(model)).predictions
-            
+
         elif 'bert' in model:
             if using_pretrained:
                 self.base_model = BertModel.from_pretrained(model)
@@ -52,14 +52,13 @@ class BERTAlignModel(pl.LightningModule):
 
             self.base_model = BertModel(AutoConfig.from_pretrained('bert-base-uncased'))
             self.discriminator_predictor = ElectraDiscriminatorPredictions(self.base_model.config)
-            
-    
+
         self.bin_layer = nn.Linear(self.base_model.config.hidden_size, 2)
         self.tri_layer = nn.Linear(self.base_model.config.hidden_size, 3)
         self.reg_layer = nn.Linear(self.base_model.config.hidden_size, 1)
 
         self.dropout = nn.Dropout(p=0.1)
-        
+
         self.need_mlm = True
         self.is_finetune = False
         self.mlm_loss_factor = 0.5
@@ -70,21 +69,24 @@ class BERTAlignModel(pl.LightningModule):
         if 'electra' in self.model:
             return self.electra_forward(batch)
         base_model_output = self.base_model(
-                input_ids = batch['input_ids'],
-                attention_mask = batch['attention_mask'],
-                token_type_ids = batch['token_type_ids'] if 'token_type_ids' in batch.keys() else None
-            )
-        
-        prediction_scores = self.mlm_head(base_model_output.last_hidden_state) ## sequence_output for mlm
-        seq_relationship_score = self.bin_layer(self.dropout(base_model_output.pooler_output)) ## pooled output for classification
+            input_ids=batch['input_ids'],
+            attention_mask=batch['attention_mask'],
+            token_type_ids=batch['token_type_ids'] if 'token_type_ids' in batch.keys() else None
+        )
+
+        prediction_scores = self.mlm_head(base_model_output.last_hidden_state)  ## sequence_output for mlm
+        seq_relationship_score = self.bin_layer(
+            self.dropout(base_model_output.pooler_output))  ## pooled output for classification
         tri_label_score = self.tri_layer(self.dropout(base_model_output.pooler_output))
         reg_label_score = self.reg_layer(base_model_output.pooler_output)
 
         total_loss = None
-        if 'mlm_label' in batch.keys(): ### 'mlm_label' and 'align_label' when training
+        if 'mlm_label' in batch.keys():  ### 'mlm_label' and 'align_label' when training
             ce_loss_fct = nn.CrossEntropyLoss(reduction='sum')
-            masked_lm_loss = ce_loss_fct(prediction_scores.view(-1, self.base_model.config.vocab_size), batch['mlm_label'].view(-1)) #/ self.con vocabulary
-            next_sentence_loss = ce_loss_fct(seq_relationship_score.view(-1, 2), batch['align_label'].view(-1)) / math.log(2)
+            masked_lm_loss = ce_loss_fct(prediction_scores.view(-1, self.base_model.config.vocab_size),
+                                         batch['mlm_label'].view(-1))  # / self.con vocabulary
+            next_sentence_loss = ce_loss_fct(seq_relationship_score.view(-1, 2),
+                                             batch['align_label'].view(-1)) / math.log(2)
             tri_label_loss = ce_loss_fct(tri_label_score.view(-1, 3), batch['tri_label'].view(-1)) / math.log(3)
             reg_label_loss = self.mse_loss(reg_label_score.view(-1), batch['reg_label'].view(-1), reduction='sum')
 
@@ -95,8 +97,10 @@ class BERTAlignModel(pl.LightningModule):
 
         return ModelOutput(
             loss=total_loss,
-            all_loss=[masked_lm_loss, next_sentence_loss, tri_label_loss, reg_label_loss]  if 'mlm_label' in batch.keys() else None,
-            loss_nums=[masked_lm_loss_num, next_sentence_loss_num, tri_label_loss_num, reg_label_loss_num] if 'mlm_label' in batch.keys() else None,
+            all_loss=[masked_lm_loss, next_sentence_loss, tri_label_loss,
+                      reg_label_loss] if 'mlm_label' in batch.keys() else None,
+            loss_nums=[masked_lm_loss_num, next_sentence_loss_num, tri_label_loss_num,
+                       reg_label_loss_num] if 'mlm_label' in batch.keys() else None,
             prediction_logits=prediction_scores,
             seq_relationship_logits=seq_relationship_score,
             tri_label_logits=tri_label_score,
@@ -109,41 +113,47 @@ class BERTAlignModel(pl.LightningModule):
         if 'mlm_label' in batch.keys():
             ce_loss_fct = nn.CrossEntropyLoss()
             generator_output = self.generator_mlm(self.generator(
-                input_ids = batch['input_ids'],
-                attention_mask = batch['attention_mask'],
-                token_type_ids = batch['token_type_ids'] if 'token_type_ids' in batch.keys() else None
+                input_ids=batch['input_ids'],
+                attention_mask=batch['attention_mask'],
+                token_type_ids=batch['token_type_ids'] if 'token_type_ids' in batch.keys() else None
             ).last_hidden_state)
-            masked_lm_loss = ce_loss_fct(generator_output.view(-1, self.generator.config.vocab_size), batch['mlm_label'].view(-1))
+            masked_lm_loss = ce_loss_fct(generator_output.view(-1, self.generator.config.vocab_size),
+                                         batch['mlm_label'].view(-1))
 
             hallucinated_tokens = batch['input_ids'].clone()
 
-            hallucinated_tokens[batch['mlm_label']!=-100] = torch.argmax(generator_output, dim=-1)[batch['mlm_label']!=-100]
-            replaced_token_label = (batch['input_ids'] == hallucinated_tokens).long()#.type(torch.LongTensor) #[batch['mlm_label'] == -100] = -100
-            replaced_token_label[batch['mlm_label']!=-100] = (batch['mlm_label'] == hallucinated_tokens)[batch['mlm_label']!=-100].long()
-            replaced_token_label[batch['input_ids'] == 0] = -100 ### ignore paddings
+            hallucinated_tokens[batch['mlm_label'] != -100] = torch.argmax(generator_output, dim=-1)[
+                batch['mlm_label'] != -100]
+            replaced_token_label = (batch[
+                                        'input_ids'] == hallucinated_tokens).long()  # .type(torch.LongTensor) #[batch['mlm_label'] == -100] = -100
+            replaced_token_label[batch['mlm_label'] != -100] = (batch['mlm_label'] == hallucinated_tokens)[
+                batch['mlm_label'] != -100].long()
+            replaced_token_label[batch['input_ids'] == 0] = -100  ### ignore paddings
 
         base_model_output = self.base_model(
-            input_ids = hallucinated_tokens if 'mlm_label' in batch.keys() else batch['input_ids'],
-            attention_mask = batch['attention_mask'],
-            token_type_ids = batch['token_type_ids'] if 'token_type_ids' in batch.keys() else None
+            input_ids=hallucinated_tokens if 'mlm_label' in batch.keys() else batch['input_ids'],
+            attention_mask=batch['attention_mask'],
+            token_type_ids=batch['token_type_ids'] if 'token_type_ids' in batch.keys() else None
         )
         hallu_detect_score = self.discriminator_predictor(base_model_output.last_hidden_state)
-        seq_relationship_score = self.bin_layer(self.dropout(base_model_output.pooler_output)) ## pooled output for classification
+        seq_relationship_score = self.bin_layer(
+            self.dropout(base_model_output.pooler_output))  ## pooled output for classification
         tri_label_score = self.tri_layer(self.dropout(base_model_output.pooler_output))
         reg_label_score = self.reg_layer(base_model_output.pooler_output)
 
         total_loss = None
 
-        if 'mlm_label' in batch.keys(): ### 'mlm_label' and 'align_label' when training
+        if 'mlm_label' in batch.keys():  ### 'mlm_label' and 'align_label' when training
             total_loss = []
             ce_loss_fct = nn.CrossEntropyLoss()
-            hallu_detect_loss = ce_loss_fct(hallu_detect_score.view(-1,2),replaced_token_label.view(-1))
+            hallu_detect_loss = ce_loss_fct(hallu_detect_score.view(-1, 2), replaced_token_label.view(-1))
             next_sentence_loss = ce_loss_fct(seq_relationship_score.view(-1, 2), batch['align_label'].view(-1))
             tri_label_loss = ce_loss_fct(tri_label_score.view(-1, 3), batch['tri_label'].view(-1))
             reg_label_loss = self.mse_loss(reg_label_score.view(-1), batch['reg_label'].view(-1))
 
             total_loss.append(10.0 * hallu_detect_loss if not torch.isnan(hallu_detect_loss).item() else 0.)
-            total_loss.append(0.2 * masked_lm_loss if (not torch.isnan(masked_lm_loss).item() and self.need_mlm) else 0.)
+            total_loss.append(
+                0.2 * masked_lm_loss if (not torch.isnan(masked_lm_loss).item() and self.need_mlm) else 0.)
             total_loss.append(next_sentence_loss if not torch.isnan(next_sentence_loss).item() else 0.)
             total_loss.append(tri_label_loss if not torch.isnan(tri_label_loss).item() else 0.)
             total_loss.append(reg_label_loss if not torch.isnan(reg_label_loss).item() else 0.)
@@ -152,7 +162,8 @@ class BERTAlignModel(pl.LightningModule):
 
         return ModelOutput(
             loss=total_loss,
-            all_loss=[masked_lm_loss, next_sentence_loss, tri_label_loss, reg_label_loss, hallu_detect_loss]  if 'mlm_label' in batch.keys() else None,
+            all_loss=[masked_lm_loss, next_sentence_loss, tri_label_loss, reg_label_loss,
+                      hallu_detect_loss] if 'mlm_label' in batch.keys() else None,
             prediction_logits=hallu_detect_score,
             seq_relationship_logits=seq_relationship_score,
             tri_label_logits=tri_label_score,
@@ -183,14 +194,14 @@ class BERTAlignModel(pl.LightningModule):
 
         total_loss = self.mlm_loss_factor * loss_mlm + loss_bin + loss_tri + loss_reg
 
-        self.log('train_loss', total_loss)# , sync_dist=True
+        self.log('train_loss', total_loss)  # , sync_dist=True
         self.log('mlm_loss', loss_mlm)
         self.log('bin_label_loss', loss_bin)
         self.log('tri_label_loss', loss_tri)
         self.log('reg_label_loss', loss_reg)
 
         return total_loss
-    
+
     def validation_step(self, val_batch, batch_idx):
         if not self.is_finetune:
             with torch.no_grad():
@@ -201,11 +212,11 @@ class BERTAlignModel(pl.LightningModule):
         with torch.no_grad():
             output = self(val_batch)['seq_relationship_logits']
             output = self.softmax(output)[:, 1].tolist()
-            pred = [int(align_prob>0.5) for align_prob in output]
+            pred = [int(align_prob > 0.5) for align_prob in output]
 
             labels = val_batch['align_label'].tolist()
 
-        return {"pred": pred, 'labels': labels}#, "preds":preds, "labels":x['labels']}
+        return {"pred": pred, 'labels': labels}  # , "preds":preds, "labels":x['labels']}
 
     def validation_step_end(self, step_output):
         losses = step_output['losses']
@@ -224,7 +235,7 @@ class BERTAlignModel(pl.LightningModule):
 
         total_loss = self.mlm_loss_factor * loss_mlm + loss_bin + loss_tri + loss_reg
 
-        self.log('train_loss', total_loss)# , sync_dist=True
+        self.log('train_loss', total_loss)  # , sync_dist=True
         self.log('mlm_loss', loss_mlm)
         self.log('bin_label_loss', loss_bin)
         self.log('tri_label_loss', loss_tri)
@@ -236,14 +247,14 @@ class BERTAlignModel(pl.LightningModule):
         if not self.is_finetune:
             total_loss = torch.stack(outputs).mean()
             self.log("val_loss", total_loss, prog_bar=True, sync_dist=True)
-        
+
         else:
             all_predictions = []
             all_labels = []
             for each_output in outputs:
                 all_predictions.extend(each_output['pred'])
                 all_labels.extend(each_output['labels'])
-            
+
             self.log("f1", f1_score(all_labels, all_predictions), prog_bar=True, sync_dist=True)
 
     def configure_optimizers(self):
@@ -271,11 +282,12 @@ class BERTAlignModel(pl.LightningModule):
 
     def mse_loss(self, input, target, ignored_index=-100.0, reduction='mean'):
         mask = (target == ignored_index)
-        out = (input[~mask]-target[~mask])**2
+        out = (input[~mask] - target[~mask]) ** 2
         if reduction == "mean":
             return out.mean()
         elif reduction == "sum":
             return out.sum()
+
 
 class ElectraDiscriminatorPredictions(nn.Module):
     """Prediction module for the discriminator, made up of two dense layers."""
@@ -294,6 +306,7 @@ class ElectraDiscriminatorPredictions(nn.Module):
         logits = self.dense_prediction(hidden_states).squeeze(-1)
 
         return logits
+
 
 @dataclass
 class ModelOutput():
